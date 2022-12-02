3601b0c6b07a722acde757081678e512d334ec31
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-12-02 14:58:12 +0200

    Add tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    described_class.new(email: 'elisha@gmail.com',
+                        encrypted_password: 'dsfsfsdfsdf')
+  end
+
+  describe 'Validations' do
+    it 'is not valid without a encrypted_password' do
+      subject.encrypted_password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without an email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe 'Associations' do
+    it { should have_many(:appointments) }
+  end
 end
